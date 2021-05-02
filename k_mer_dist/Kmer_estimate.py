bd73dde3153e9d67db789b94599fa736156c4b55
raquellewei <raquelle70679@gmail.com>
2021-05-02 14:50:38 -0400

    first commit

--- k_mer_dist/Kmer_estimate.py
+++ k_mer_dist/Kmer_estimate.py
@@ -4,7 +4,6 @@ sys.path.insert(1, '/Users/weiwei/PycharmProjects/pythonProject/CmashKmerAbundan
 sys.path.insert(1, '/Users/weiwei/PycharmProjects/pythonProject/CmashKmerAbundance/KMC/bin')
 import py_kmc_api as kmc
 from scipy.stats import wasserstein_distance
-import math
 import pandas as pd
 import numpy as np
 from CMash import MinHash as MH
@@ -12,7 +11,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle
 from copy import deepcopy
-import random
+import time
 
 def get_MH_data(n, k, genome_file, rev_comp=False):
     '''
@@ -49,7 +48,7 @@ def get_kmc_data(k, genome_file, out_file, out_dir, verbose=False):
     kmc_database = kmc.KMCFile()
     abs_path = os.getcwd()
     out_path = abs_path+'/'+out_dir+'/'+out_file
-    os.system('kmc -k%d -cs3000 -b -v -fm -ci0 %s %s %s' %(k, genome_file, out_path , '.'))
+    os.system('kmc -k%d -cs1000 -b -v -ci2 %s %s %s' %(k, genome_file, out_path , '.'))
     kmc_database.OpenForListing(out_path)
     kmer_obj = kmc.KmerAPI(kmc_database.Info().kmer_length)
     counter = kmc.Count()
@@ -70,7 +69,7 @@ def get_kmc_data(k, genome_file, out_file, out_dir, verbose=False):
     #print(df)
     #sns.histplot(x='kmer_count', y='percentage', binwidth=1, data=df)
     #plt.savefig('test1.png')
-    #print(normed_dict)
+    print(normed_dict)
     return normed_dict
 
 def get_distance(dict1, dict2, type='L1'):
@@ -117,7 +116,7 @@ def get_distance(dict1, dict2, type='L1'):
 def get_count_dict(count_list):
     count_dict = dict()
     for count in count_list:
-        if count > 0:
+        if count > 1:
             if count in count_dict.keys():
                 count_dict[count] += 1
             else:
@@ -156,20 +155,39 @@ def get_n_vs_dist_dataframe(k, input_file, n):
 
 def how_is_k_doing(n, kmin, kmax, kstep, input_file, outfile):
     k_list = list(range(kmin,kmax,kstep))
-    df = pd.DataFrame(columns=['k', 'L1', 'L2', 'ratio'], index=k_list)
-    df['k'] = k_list
+    df_mh = pd.DataFrame(columns=['k', 'L1', 'L2', 'ratio', 'wasserstein', 'method'], index=k_list)
+    df_ntcard = pd.DataFrame(columns=['k', 'L1', 'L2', 'ratio', 'wasserstein', 'method'], index=k_list)
+    df_mh['method'] = "Cmash"
+    df_ntcard['method'] = "ntCard"
+    df_mh['k'] = k_list
+    df_ntcard['k'] = k_list
     for k in k_list:
         pickle_name='k' + str(k) + 'n10000' + input_file + '.pickle'
+        os.system('ntcard -k%d -p %s %s' %(k, 'srrntcard', input_file))
+        time.sleep(10)
         with open(pickle_name, 'rb') as pf:
             count_list = pickle.load(pf)
         estimated_normed_dict = get_count_dict(count_list)
         kmc_normed_dict = get_kmc_data(k, input_file, input_file + '_out', 'out')
-        L1 = get_distance(kmc_normed_dict, estimated_normed_dict, 'L1')
-        L2 = get_distance(kmc_normed_dict, estimated_normed_dict, 'L2')
-        ratio = get_distance(kmc_normed_dict, estimated_normed_dict, 'ratio')
-        df.at[k, 'L1'] = L1
-        df.at[k, 'L2'] = L2
-        df.at[k, 'ratio'] = ratio
+        mhL1 = get_distance(kmc_normed_dict, estimated_normed_dict, 'L1')
+        mhL2 = get_distance(kmc_normed_dict, estimated_normed_dict, 'L2')
+        mhratio = get_distance(kmc_normed_dict, estimated_normed_dict, 'ratio')
+        mhwasser = get_distance(kmc_normed_dict, estimated_normed_dict, 'wasserstein')
+        df_mh.at[k, 'L1'] = mhL1
+        df_mh.at[k, 'L2'] = mhL2
+        df_mh.at[k, 'ratio'] = mhratio
+        df_mh.at[k, 'wasserstein'] = mhwasser
+        nt_out = 'srrntcard' + '_k' + str(k) + '.hist'
+        nt_dist = parse_ntcard_output(nt_out)
+        ntL1 = get_distance(kmc_normed_dict, nt_dist, 'L1')
+        ntL2 = get_distance(kmc_normed_dict, nt_dist, 'L2')
+        ntratio = get_distance(kmc_normed_dict, nt_dist, 'ratio')
+        ntwasser = get_distance(kmc_normed_dict, nt_dist, 'wasserstein')
+        df_ntcard.at[k, 'L1'] = ntL1
+        df_ntcard.at[k, 'L2'] = ntL2
+        df_ntcard.at[k, 'ratio'] = ntratio
+        df_ntcard.at[k, 'wasserstein'] = ntwasser
+    df = pd.concat([df_mh, df_ntcard])
     df.to_csv(outfile, sep='\t', index=True)
     print(df)
 
@@ -177,8 +195,16 @@ def kmc_cmash_compare(k, n, input_file):
     kmc_normed_dict = get_kmc_data(k, input_file, input_file+'_out', 'out')
     #minhash estimate
     estimator = MH.CountEstimator(n=n, ksize=k, save_kmers='n', input_file_name=input_file)
+    real_dist = pd.DataFrame(list(kmc_normed_dict.items()), columns=['kmer_count', 'percentage'])
+    sns.barplot(x='kmer_count', y='percentage', data=real_dist)
+    plt.savefig('quicklook_real.png')
     counts = estimator._counts
     estimated_normed_dict = get_count_dict(counts)
+    #quick look at distribution
+    df = pd.DataFrame(list(estimated_normed_dict.items()), columns=['kmer_count', 'percentage'])
+    sns.barplot(x='kmer_count', y='percentage', data=df)
+    plt.savefig('quicklook.png')
+    #####
     print(sum(estimated_normed_dict.values()))
     print(counts)
     print(get_distance(kmc_normed_dict, estimated_normed_dict, 'wasserstein'))
@@ -186,7 +212,7 @@ def kmc_cmash_compare(k, n, input_file):
 
 def quicker_dump(input_file):
     n=10000
-    for k in list(range(7, 74, 5)):
+    for k in [25, 50,75]:
         pickle_file = 'k' + str(k) + 'n10000' + input_file + '.pickle'
         print(pickle_file)
         estimator = MH.CountEstimator(n=n, ksize=k, save_kmers='n', input_file_name=input_file, rev_comp=False)
@@ -194,6 +220,65 @@ def quicker_dump(input_file):
         with open(pickle_file, 'wb') as pf:
             pickle.dump(counts, pf)
 
+def parse_ntcard_output(file):
+    nt_dict = dict()
+    with open(file, 'r') as f:
+        f.readline()
+        f.readline()
+        f.readline()
+        for line in f.readlines():
+            line = line.strip()
+            dist = line.split('\t')
+            nt_dict[int(dist[0])] = int(dist[1])
+        normed_dict = dict()
+        total_count = sum(nt_dict.values())
+        for k, v in nt_dict.items():
+            normed_dict[k] = nt_dict[k] / total_count
+        return normed_dict
+
+
+def test_bb_batch():
+    n_range = [100, 500, 1000, 10000]
+    k25df = pd.DataFrame(columns=['n', 'L2', 'k'])
+    k50df = pd.DataFrame(columns=['n', 'L2', 'k'])
+    k75df = pd.DataFrame(columns=['n', 'L2', 'k'])
+    n_col = []
+    for n in n_range:
+        n_col = n_col + [n]*10
+    k25df['n'] = n_col
+    k50df['n'] = n_col
+    k75df['n'] = n_col
+    k25df['k'] = 25
+    k50df['k'] = 50
+    k75df['k'] = 75
+    k25df['L2'] = _get_L2_col(25, n_range)
+    k50df['L2'] = _get_L2_col(50, n_range)
+    k75df['L2'] = _get_L2_col(75, n_range)
+    df_combined = pd.concat([k25df, k50df, k75df])
+    df_combined.to_csv('df_for_boxplot.txt', sep="\t")
+
+def _get_L2_col(k, n_range):
+    L2_col = []
+    for n in n_range:
+        #10 repeats
+        for i in range(10):
+            bbfile = 'bbsim' + str(i+1) + '.fasta'
+            kmc_normed_dict = get_kmc_data(k, bbfile, bbfile + '_out', 'out')
+            pickle_file = 'k'+str(k) + 'n10000' + bbfile + '.pickle'
+            with open(pickle_file, 'rb') as pf:
+                count_list = pickle.load(pf)
+            estimated_normed_dict = get_count_dict(count_list[0:n])
+            L2 = get_distance(kmc_normed_dict, estimated_normed_dict, 'L2')
+            print("n=%d k=%d L2=%f " %(n, k, L2))
+            L2_col.append(L2)
+    return L2_col
+
+def get_boxplot_from_file(file, x, y, hue, outfile):
+    df = pd.read_table(file)
+    #sns.boxplot(x="n", y="L2", hue="k", data=df)
+    #plt.savefig("L2_against_n.png")
+    sns.lineplot(x=x, y=y, data=df, hue=hue)
+    plt.savefig(outfile)
 
 if __name__ == "__main__":
     #kmc_result = get_kmc_data(60, 'SRR172902_1.fastq', 'SRR172902_out', 'out')
@@ -206,4 +291,11 @@ if __name__ == "__main__":
     #for k in [15, 21, 30, 48, 60, 70]:
     #    get_n_vs_dist_dataframe(k, 'SRR172902_1.fastq', n)
     #quicker_dump('SRR172902_1.fastq')
-    kmc_cmash_compare(25, 1000, 'bbsim2.fasta')
+    #kmc_cmash_compare(25, 1000, 'bbsim2.fasta')
+    #for i in range(9):
+    #    file = 'bbsim' + str(i+1) + '.fasta'
+    #    quicker_dump(file)
+    #test_bb_batch()
+    #get_boxplot_from_file('df_for_boxplot.txt')
+    #how_is_k_doing(1000, 7, 75, 5, 'SRR172902_1.fastq', 'howsk.txt')
+    get_boxplot_from_file('howsk.txt', 'k', 'wasserstein', 'method', "k_agains_wasserstein.png")
